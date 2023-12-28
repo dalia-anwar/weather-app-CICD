9a81cde71e8d5c18affdf2ab0d7f1c700dc86695
Dalia Anwar <dalia.anwar@valeo.com>
2023-12-28 19:23:01 +0200

    adds terraform to automate jenkins-slave provisioning

---
+++ terraform/jenkins_master.tf
@@ -0,0 +1,62 @@
+data "aws_ami" "jenkins_master" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["jenkins-master"]
+  }
+}
+
+  
+resource "aws_security_group" "jenkins_master_sg" {
+  name        = "jenkins_master_sg"
+  description = "Allow traffic on port 8080 and enable SSH"
+
+  ingress {
+    from_port       = "22"
+    to_port         = "22"
+    protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = "8080"
+    to_port         = "8080"
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jenkins_elb_sg.id]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name   = "jenkins_master_sg"
+  }
+}
+data "template_file" "jenkins_master_user_data" {
+  template = file("user-data-jenkins-master.sh")
+}
+
+resource "aws_instance" "jenkins_master" {
+  ami                    = "ami-024f768332f080c5e"
+  instance_type          = var.jenkins_master_instance_type
+  key_name               = var.key
+  vpc_security_group_ids = [aws_security_group.jenkins_master_sg.id]
+  user_data              = data.template_file.jenkins_master_user_data.rendered
+
+  root_block_device {
+    volume_type           = "gp3"
+    volume_size           = 10
+    delete_on_termination = false
+  }
+
+  tags = {
+    Name   = "jenkins_master"
+  }
+}
---
+++ terraform/user-data-jenkins-master.sh
@@ -0,0 +1,29 @@
+
+echo "Install git"
+yum install -y git
+
+###################################
+
+echo "Install Jenkins LTS"
+sudo yum update -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo dnf install java-17-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+####################################
+
+sudo yum update -y
+sudo yum install docker -y
+sudo systemctl start docker
+sudo docker run hello-world
+sudo systemctl enable docker
+docker --version
+sudo usermod -a -G docker $(whoami)
+newgrp docker
+
+####################################
